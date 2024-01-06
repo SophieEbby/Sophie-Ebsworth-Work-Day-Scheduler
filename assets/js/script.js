45926e2314d547fb958a4b50176bdb63f83c8941
Sophie <149930661+SophieEbby@users.noreply.github.com>
2024-01-06 15:42:28 +0000

    Add event listener for save button, saves to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,13 @@
 
 //!Present time blocks for standard business hours (9AM - 5PM) when the user scrolls down - need a function to render a table format (3 columns, 9 Rows) using the div.container
 
-//Within the same function, need to colour code each time block based on past, present, and future when the time block is viewed - using the classes past, present and future.
-//Past - Grey colour, Present - Red colour, Future - Green colour
+//!Within the same function, need to colour code each time block based on past, present, and future when the time block is viewed - using the classes past, present and future.
+//!Past - Grey colour, Present - Red colour, Future - Green colour
 //Need to check the time constantly to see if the colour blocks need to change.
 
-//Allow a user to enter an event when they click a time block
+//!Allow a user to enter an event when they click a time block
 
-//Save the event in local storage when the save button is clicked in that time block - event listener 
+//!Save the event in local storage when the save button is clicked in that time block - event listener 
 
 //Persist events between refreshes of a page - need to load events from local storage
 
@@ -57,18 +57,29 @@ $(document).ready(function () {
 
             // set calendar render time
             hourRendered = dayjs();
+        };
     };
-};
 
     // initialize calendar
     function initCalendar() {
         const today = dayjs(); // set today's date
         currentDateEl.text(today.format('dddd, D MMMM YYYY'));
         renderCalendar(today, calEvents); //calls renderCalendar function
-        };
+    };
 
-        // When the page loads:
-        initCalendar(); // set the current date
+    // When the page loads:
+    initCalendar(); // set the current date
 
+    // store calendar events in local storage
+    function storeCal() {
+        localStorage.setItem("calEvents", JSON.stringify(calEvents));
+    };
 
+    // Save button click handler - save calendar event 
+    $(document).on("click", "button.saveBtn", function (event) {
+        let calDesc = event.currentTarget.parentElement.children[1].value; // store contents of that row's textarea
+        calEvents[event.currentTarget.id] = calDesc; // add calendar event text to the calEvents object with the time as the key
+        storeCal(); // store the calEvents in local storage
     });
+
+});
